eaba8a7daea44e9c1df069c2cc0af4629a2cbb3f
chenduo1999 <2198727176@qq.com>
2024-09-22 20:22:37 +0800

    更新说明

---
+++ snippet.cpp
@@ -0,0 +1,37 @@
+/**
+ * struct ListNode {
+ *	int val;
+ *	struct ListNode *next;
+ *	ListNode(int x) : val(x), next(nullptr) {}
+ * };
+ */
+class Solution {
+public:
+    /**
+     * 代码中的类名、方法名、参数名已经指定，请勿修改，直接返回方法规定的值即可
+     *
+     * 
+     * @param head ListNode类 
+     * @return ListNode类
+     */
+    ListNode* ReverseList(ListNode* head) {
+        // write code here
+        
+        ListNode* pre = nullptr;
+        ListNode* curr =head;
+        ListNode* next;
+            if(head==nullptr){
+                return head;
+            }else {
+                while(curr){
+                    next=curr->next;
+                    curr->next =pre;
+                    pre=curr;
+                    curr=next;
+                }
+
+                 return pre;
+            }
+            
+    }
+};
